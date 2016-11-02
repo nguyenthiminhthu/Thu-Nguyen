27968dcc659758c3766a6864a298f15e57cf6a5c
nguyen thi minh thu <nguyenthiminhthu.vlu96@gmail.com>
2016-11-02 17:18:13 +0700

    init project

---
+++ GuiDemo/src/GuiDemo.java
@@ -0,0 +1,21 @@
+import javax.swing.JFrame;
+
+/**
+ * 
+ */
+
+/**
+ * @author US
+ *
+ */
+public class GuiDemo extends JFrame {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
